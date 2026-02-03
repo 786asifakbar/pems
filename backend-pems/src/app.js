1a2658105da762bbde6de11693b0bd4ebf49ca55
Asif Akbar <786asifakbar53@gmail.com>
2026-02-04 02:33:09 +0500

    fix: update import paths and add cors dependency

--- backend-pems/src/app.js
+++ backend-pems/src/app.js
@@ -1,4 +1,6 @@
+import cors from "cors"
 import express from "express"
+
 
 const app = express();
 
--- backend-pems/src/server.js
+++ backend-pems/src/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import app from "app.js"
-dotenv.config({ path: "./.env" })
+import app from "./app.js"
+dotenv.config({path: "./.env"});
 
 connectDB()
     .then(() => {
