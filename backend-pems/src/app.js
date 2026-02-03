ea29c00e550b66989227871860e53b2fe57f3c2e
Asif Akbar <786asifakbar53@gmail.com>
2026-02-04 03:04:29 +0500

    fix: add cookie-parser dependency and update app.js to use it

--- backend-pems/src/app.js
+++ backend-pems/src/app.js
@@ -1,6 +1,6 @@
 import cors from "cors";
 import express from "express"
-
+import  cookieParser from "cookie-parser";
 
 const app = express();
 
@@ -11,5 +11,8 @@ app.use(cors(
     }
 ))
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 export default app;
